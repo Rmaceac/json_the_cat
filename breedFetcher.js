0678606f57501a58f90cf01c0cd7e7b76a39295b
Ryan <ryan.maceachern@gmail.com>
2022-02-17 15:50:50 -0800

    init repo && completed basic functionality of breedFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const userQuery = process.argv[2];
+// console.log("User Input: ", userQuery);
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${userQuery}`;
+
+// if (!userQuery) {
+//   return "Error: no search parameter given";
+// }
+request(URL, (error, response, body) => {
+  const data = JSON.parse(body);
+  //if an error occurs, prints basic error information
+  if (error) {
+    console.log('error:', error);
+    console.log('status code:', response && response.statusCode);
+  }
+  
+  // prints an error and exits the code if the breed is not found (no breed found on request returns an empty array).
+  if (data.length === 0) {
+    console.log("Error: Breed not found");
+    return;
+  }
+  console.log(data[0].description);
+});
